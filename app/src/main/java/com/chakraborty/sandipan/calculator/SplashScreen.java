e90c8477b45893f004383ff21700897b2bd33701
sandipan144 <sandipanchakraborty144@gmail.com>
2017-12-31 14:40:17 +0530

    animation

--- app/src/main/java/com/chakraborty/sandipan/calculator/SplashScreen.java
+++ app/src/main/java/com/chakraborty/sandipan/calculator/SplashScreen.java
@@ -5,12 +5,15 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
